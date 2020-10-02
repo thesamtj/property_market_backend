e21ae9a476059af2e4559b5c56749e07b26bad5b
Samuel Tijani <omalitijani@gmail.com>
2020-10-02 14:20:00 +0100

    Use dependency injection

--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using property_market_backend.Data;
 
 namespace property_market_backend
 {
@@ -24,6 +26,8 @@ namespace property_market_backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllers();
             services.AddCors();
         }
