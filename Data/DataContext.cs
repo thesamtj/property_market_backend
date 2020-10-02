276d68585b08230f4395c5e9234d4d85dd70e772
Samuel Tijani <omalitijani@gmail.com>
2020-10-02 14:07:03 +0100

    Define DbContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using property_market_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace property_market_backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<City> Cities { get; set; }
+
+    }
+}
