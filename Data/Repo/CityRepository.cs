dc6b0c5be96e30284917c5f8cc7d0a81cb7d3ee2
Samuel Tijani <omalitijani@gmail.com>
2020-10-05 14:52:06 +0100

    Implement city repo

---
+++ Data/Repo/CityRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using property_market_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace property_market_backend.Data.Repo
+{
+    public class CityRepository : ICityRepository
+    {
+        private readonly DataContext dc;
+
+        public CityRepository(DataContext dc)
+        {
+            this.dc = dc;
+        }
+        public void AddCity(City city)
+        {
+            dc.Cities.AddAsync(city);
+        }
+
+        public void DeleteCity(int CityId)
+        {
+            var city = dc.Cities.Find(CityId);
+            dc.Cities.Remove(city);
+        }
+
+        public async Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            return await dc.Cities.ToListAsync();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await dc.SaveChangesAsync() > 0;
+        }
+    }
+}
