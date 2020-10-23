c21014a60dd9d1c05e5b7eda3f6471a98d803a60
Samuel Tijani <omalitijani@gmail.com>
2020-10-23 11:58:56 +0100

    Define uow interface

---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace property_market_backend.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ICityRepository CityRepository { get; }
+        Task<bool> SaveAsync();
+    }
+}
