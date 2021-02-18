15b9b30314d0f1190c3228524ea05bf28faeb024
Samuel Tijani <omalitijani@gmail.com>
2021-02-18 18:59:27 +0100

    throw an exception

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -28,6 +28,7 @@ namespace property_market_backend.Controllers
         [HttpGet]
         public async Task<IActionResult> GetCities()
         {
+            throw new UnauthorizedAccessException();
             var cities = await uow.CityRepository.GetCitiesAsync();
 
             var citiesDto = mapper.Map<IEnumerable<CityDto>>(cities);
