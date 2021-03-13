115fc1d7a33a4e48b61d38ca231af8aee3be9110
Samuel Tijani <omalitijani@gmail.com>
2021-03-13 16:24:39 +0100

    Apply to middleware

--- Errors/ApiError.cs
+++ Errors/ApiError.cs
@@ -7,5 +7,17 @@ namespace property_market_backend.Errors
 {
     public class ApiError
     {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
+
+        public ApiError(int errorCode, string errorMessage, string errorDetails = null)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+            ErrorDetails = errorDetails;
+        }
+
+
     }
 }
--- Middlewares/ExceptionMiddleware.cs
+++ Middlewares/ExceptionMiddleware.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using property_market_backend.Errors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace property_market_backend.Middlewares
@@ -11,11 +14,13 @@ namespace property_market_backend.Middlewares
     {
         private readonly RequestDelegate next;
         private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IHostEnvironment env;
 
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
         {
             this.next = next;
             this.logger = logger;
+            this.env = env;
         }
 
         public async Task Invoke(HttpContext context)
@@ -26,9 +31,20 @@ namespace property_market_backend.Middlewares
             }
             catch (Exception ex)
             {
+                ApiError response;
+                HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+                if (env.IsDevelopment())
+                {
+                    response = new ApiError((int)statusCode, ex.Message, ex.StackTrace);
+                } else
+                {
+                    response = new ApiError((int)statusCode, ex.Message);
+                }
+
                 logger.LogError(ex, ex.Message);
-                context.Response.StatusCode = 500;
-                await context.Response.WriteAsync(ex.Message);
+                context.Response.StatusCode = (int)statusCode;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(response.ToString());
             }
         }
     }
