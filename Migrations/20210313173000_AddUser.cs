574f3d45cf301182ee5095fe403923b23ffd9199
Samuel Tijani <omalitijani@gmail.com>
2021-03-13 18:43:03 +0100

    Make required

---
+++ Migrations/20210313173000_AddUser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace property_market_backend.Migrations
+{
+    public partial class AddUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Cities",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastUpdatedBy",
+                table: "Cities",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedOn",
+                table: "Cities",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<int>(nullable: false),
+                    Password = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedOn",
+                table: "Cities");
+        }
+    }
+}
