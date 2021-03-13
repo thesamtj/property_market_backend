cdc77d062889dfafc02e51c43c13b86eaa33bf22
Samuel Tijani <omalitijani@gmail.com>
2021-03-13 17:58:59 +0100

    Create user model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace property_market_backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Username { get; set; }
+        public int Password { get; set; }
+    }
+}
