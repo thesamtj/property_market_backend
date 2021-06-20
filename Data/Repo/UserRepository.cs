dc5366fba27b21faa474476abb61ef4e3d26eb66
Samuel Tijani <omalitijani@gmail.com>
2021-06-20 15:40:06 +0100

    Create User repo

---
+++ Data/Repo/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using property_market_backend.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace property_market_backend.Data.Repo
+{
+    public class UserRepository : IUserRepository
+    {
+    }
+}
