922a3e2d36cebb291a6f3bca708ca444af950f48
Samuel Tijani <omalitijani@gmail.com>
2021-06-20 15:31:37 +0100

    Create User interface repo

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace property_market_backend.Interfaces
+{
+    public interface IUserRepository
+    {
+
+    }
+}
