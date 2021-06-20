e8cfd2a8f0371741c9ea9600680654902dda1167
Samuel Tijani <omalitijani@gmail.com>
2021-06-20 15:33:00 +0100

    Add authenticate method signature

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using property_market_backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace property_market_backend.Interfaces
 {
     public interface IUserRepository
     {
-
+        Task<User> Authenticate(string userName, string password);
     }
 }
