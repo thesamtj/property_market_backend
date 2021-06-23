2390ff6a0f7f331d8f828c33477b65fc2a11062f
Samuel Tijani <omalitijani@gmail.com>
2021-06-23 18:34:58 +0100

    Implement methods

--- Data/Repo/UserRepository.cs
+++ Data/Repo/UserRepository.cs
@@ -4,6 +4,7 @@ using property_market_backend.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace property_market_backend.Data.Repo
@@ -20,6 +21,29 @@ namespace property_market_backend.Data.Repo
         public async Task<User> Authenticate(string userName, string password)
         {
             return await dc.Users.FirstOrDefaultAsync(x => x.Username == userName /*&& x.Password == password*/);
+        }
+
+        public void Register(string userName, string password)
+        {
+            byte[] passwordHash, passwordKey;
+
+            using (var hmac = new HMACSHA512())
+            {
+                passwordKey = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+            User user = new User();
+            user.Username = userName;
+            user.Password = passwordHash;
+            user.PasswordKey = passwordKey;
+
+            dc.Users.Add(user);
+        }
+
+        public async Task<bool> UserAlreadyExists(string userName)
+        {
+            return await dc.Users.AnyAsync(x => x.Username == userName);
         }
     }
 }
